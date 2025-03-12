01173a5929bb76234315ff7e9bc545d9cb040daf
tk1 <thuan2609.tk1@gmail.com>
2025-03-12 08:27:54 +0700

    Update Logic

--- ios-notes/AppDelegate.swift
+++ ios-notes/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+        setupFirebase()
         return true
     }
     
@@ -23,5 +23,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration",
                                     sessionRole: connectingSceneSession.role)
+    }
+}
+
+extension AppDelegate {
+    private func setupFirebase() {
+        // Please setup Gooogle API_KEY & GOOGLE_APP_ID before open comment below
+        // FirebaseApp.configure()
     }
 }
--- ios-notes/Data/FireBase/FireBaseService.swift
+++ ios-notes/Data/FireBase/FireBaseService.swift
@@ -19,47 +19,56 @@ struct FirebaseService { }
 
 extension FirebaseService: FirebaseServiceType {
     func getNotes() -> Observable<[Note]> {
+        // Simple func fetch data from Remote DB
+        // In practice we need to design DB with more detailed fields
+        
+        
         return Observable.create { observer in
-            Firestore.firestore().collection("notes-collection")
-                .document("5zwvvSCPmciO0UhAOQiM")
-                .getDocument { (document, _) in
-                    guard let document = document, document.exists else {
-                        observer.onNext([])
-                        observer.onCompleted()
-                        return
-                    }
-                    
-                    let data = document.data()?["data"] as? [[String: Any]] ?? []
-                    let notes = data.mapToNotes()
-                    
-                    observer.onNext(notes)
-                    observer.onCompleted()
-                }
+//            Firestore.firestore().collection("notes-collection")
+//                .document("5zwvvSCPmciO0UhAOQiM")
+//                .getDocument { (document, _) in
+//                    guard let document = document, document.exists else {
+//                        observer.onNext([])
+//                        observer.onCompleted()
+//                        return
+//                    }
+//                    
+//                    let data = document.data()?["data"] as? [[String: Any]] ?? []
+//                    let notes = data.mapToNotes()
+//                    
+//                    observer.onNext(notes)
+//                    observer.onCompleted()
+//                }
             
+            
+            observer.onNext([])
+            observer.onCompleted()
             return Disposables.create()
         }
     }
     
     func saveSyncNotes(_ notes: [Note]) {
-        let collectionRef = Firestore.firestore().collection("notes-collection")
-        let documentRef = collectionRef.document("5zwvvSCPmciO0UhAOQiM")
-        let validNotes = notes.filter { !$0.isDeleted }
+        // Simple func save data to Remote DB
         
-        let notesData: [[String: Any]] = validNotes.map { note in
-            return [
-                "id": note.id ?? "",
-                "title": note.title ?? "",
-                "content": note.content ?? "",
-                "updatedAt": note.updatedAt?.toString(format: .dayMonthYearHour) ?? ""
-            ]
-        }
-
-        documentRef.setData(["data": notesData], merge: true) { error in
-            if let error = error {
-                print("Error saving notes:", error.localizedDescription)
-            } else {
-                print("Notes saved successfully!")
-            }
-        }
+//        let collectionRef = Firestore.firestore().collection("notes-collection")
+//        let documentRef = collectionRef.document("5zwvvSCPmciO0UhAOQiM")
+//        let validNotes = notes.filter { !$0.isDeleted }
+//        
+//        let notesData: [[String: Any]] = validNotes.map { note in
+//            return [
+//                "id": note.id ?? "",
+//                "title": note.title ?? "",
+//                "content": note.content ?? "",
+//                "updatedAt": note.updatedAt?.toString(format: .dayMonthYearHour) ?? ""
+//            ]
+//        }
+//
+//        documentRef.setData(["data": notesData], merge: true) { error in
+//            if let error = error {
+//                print("Error saving notes:", error.localizedDescription)
+//            } else {
+//                print("Notes saved successfully!")
+//            }
+//        }
     }
 }
